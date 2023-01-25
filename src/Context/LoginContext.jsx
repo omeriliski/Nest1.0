24e8ea94fb988d8a69ed497b10b248dbf9be6ae9
Aysegül Iliski <omeriliski06@gmail.com>
2023-01-25 15:27:31 +0100

    first commit

--- src/Context/LoginContext.jsx
+++ src/Context/LoginContext.jsx
@@ -25,6 +25,7 @@ export default function LoginContextProvider(props){
 
     const login=()=>{
         console.log('JSON.stringify(env()) :>> ', JSON.stringify(env()));
+        console.log('env() :>> ', env());
         // axios.post(`${env.REACT_APP_URL}/api/user/login`, {
             axios.post(`https://nestbackend-sjpjiklsqa-ey.a.run.app/api/user/login`, {
             email: email,
