cfa03bcbbcb2845a8d48607a0d6e5f5050a66233
Aysegül Iliski <omeriliski06@gmail.com>
2023-02-12 00:16:41 +0100

    fixed login url

--- src/Context/LoginContext.jsx
+++ src/Context/LoginContext.jsx
@@ -27,8 +27,8 @@ export default function LoginContextProvider(props){
     const login=()=>{
         console.log('`${process.env.REACT_APP_URL}/api/user/login` :>> ', `${process.env.REACT_APP_URL}/api/user/login`);
         // console.log("env('REACT_APP_URL')",env("REACT_APP_URL"));
-        axios.post(`/api/user/login`, {
-            //axios.post(`https://nestbackend-sjpjiklsqa-ey.a.run.app/api/user/login`, {
+        //axios.post(`/api/user/login`, {
+        axios.post(`https://nestbackend-sjpjiklsqa-ey.a.run.app/api/user/login`, {
             email: email,
             password: password
         })
@@ -49,8 +49,8 @@ export default function LoginContextProvider(props){
 
     const registerFunction = (values)=>{
         console.log("values", values);
-        axios.post(`/api/user/register`, {
-        //axios.post(`https://nestbackend-sjpjiklsqa-ey.a.run.app/api/user/register`, {
+        //axios.post(`/api/user/register`, {
+        axios.post(`https://nestbackend-sjpjiklsqa-ey.a.run.app/api/user/register`, {
             loginInfo: 
             {
                 email:values.email,
